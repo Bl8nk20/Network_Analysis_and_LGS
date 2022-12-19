8a49a48aaaa0a65a8dc8888dcdf56568bbcd0a18
Bl8nk20 <94741379+Bl8nk20@users.noreply.github.com>
2022-12-19 13:03:39 +0100

    Status: 12.19.2022-13:03

---
+++ LGS_Solving/LGS_Solving_Libary/Class1.cs
@@ -0,0 +1,21 @@
+﻿namespace LGS_Solving_Libary
+{
+    interface IDataGathering<T>
+    {
+        IEnumerable<T> GetData();
+    }
+
+    interface IDataManipulatior<T, V>
+    {
+        V Manipulate(T data);
+    }
+
+    interface IDataWriter<T>
+    {
+        void Write(T data);
+    }
+    public class Class1
+    {
+
+    }
+}
