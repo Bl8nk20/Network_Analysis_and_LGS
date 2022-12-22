4f656dd136ab070487cbb9bd1f106b4fac17e85e
Bl8nk20 <94741379+Bl8nk20@users.noreply.github.com>
2022-12-22 10:18:03 +0100

    Status: 12.22.2022 - 10:17

---
+++ LGS_Solving/Electrical_Components/CircuitComponent.cs
@@ -0,0 +1,13 @@
+﻿using System.Windows.Controls;
+
+namespace CircuitSimulator
+{
+    public abstract class CircuitComponent
+    {
+        // Abstract method to draw the component on the canvas
+        public abstract void Draw(Canvas canvas, double x, double y);
+
+        // Abstract method to calculate the voltage and current for the component
+        public abstract void Calculate(double voltage);
+    }
+}
