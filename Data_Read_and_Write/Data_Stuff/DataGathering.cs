77dcad0eeb0a887af249d34eada7b202268c8c3c
Bl8nk20 <94741379+Bl8nk20@users.noreply.github.com>
2023-01-14 16:09:42 +0100

    Add files via upload

---
+++ Data_Read_and_Write/Data_Stuff/DataGathering.cs
@@ -0,0 +1,19 @@
+﻿using Data_Read_and_Write;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data_Read_and_Write.Data_Stuff;
+
+/// <summary>
+/// 
+/// </summary>
+public class DataGathering
+{
+    public double Voltage { get; set; }
+    public double Resistance { get; set; }    
+    public double Current { get; set; }
+
+}
---
+++ Data_Read_and_Write/Data_Stuff/Writing.cs
@@ -0,0 +1,45 @@
+﻿using CsvHelper;
+using System.Globalization;
+
+namespace Data_Read_and_Write.Data_Stuff;
+
+public class Writing
+{
+    internal double[] voltages;
+    internal double[] resistances;
+    internal double[] currents;
+    internal int amountResistances;
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="voltages"></param>
+    /// <param name="resistances"></param>
+    /// <param name="currents"></param>
+    public Writing(double[] voltages, double[] resistances, double[] currents)
+    {
+        this.voltages = voltages;
+        this.resistances = resistances;
+        this.currents = currents;
+        amountResistances = resistances.Length;
+    }
+    public void WritingFile()
+    {
+        var Collection = new List<DataGathering>();
+
+        for (int i = 0; i < amountResistances; i++)
+        {
+            Collection.Add(new DataGathering { Voltage = voltages[i], Resistance = resistances[i], Current = currents[i] });
+        }
+
+        using (var writer = new StreamWriter("values.csv"))
+        using(var csv = new CsvWriter(writer, CultureInfo.InvariantCulture))
+        {
+            csv.WriteRecords(Collection);
+        }
+
+    }
+
+    ~Writing()
+    { }
+}
